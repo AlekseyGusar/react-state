9caf77a9ceba88fec984b94f6fb6c28f4f4c48f4
AlekseyGusar <aleksey.gusar.2@gmail.com>
2021-04-17 21:05:04 +0300

    add event handler

--- src/App.js
+++ src/App.js
@@ -2,8 +2,15 @@ import React from 'react';
 import './App.css';
 
 export default class App extends React.Component {
-  
+  constructor(props) {
+    super(props);
+    this.state = {appData: 'React Marathon'};
+    this.handleClick = this.handleClick.bind(this);
+  }
+    handleClick() {
+      this.setState({ appData: this.state.appData.toLowerCase() })
+   }
   render() {
-    return <div> React Marathon </div>    
+    return <div onClick={this.handleClick}> {this.state.appData} </div>
   }
 }
